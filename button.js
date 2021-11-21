e990ed8c2657851998354b84af66713e62a003b4
Svolson <93822517+Svolson@users.noreply.github.com>
2021-11-21 21:13:36 +0200

    Add files via upload

---
+++ button.js
@@ -0,0 +1,7 @@
+function show_table()
+{
+    if (document.getElementById("Student_Information").style.display === "none")
+        document.getElementById("Student_Information").style.display="block";
+    else
+        document.getElementById("Student_Information").style.display="none";
+}
